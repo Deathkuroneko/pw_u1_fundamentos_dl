f37515c245807fece7dedfab68bf5dbfcfd77587
Deathkuroneko <deathkuroneko@hotmail.com>
2025-11-07 19:39:36 -0500

    Taller 7 - Primera parte de la Calculadora

---
+++ js/calculadoraFinal.js
@@ -0,0 +1,25 @@
+let num1 = document.getElementById('1');
+let num2 = 0;
+
+
+function mostraDisplay(valor){
+    let elemento = document.getElementById('display');
+    elemento.innerText = elemento.innerText + valor;
+}
+
+function sumar(valor){
+    let sumados = document.getElementById('+')
+
+}
+function restar(){
+    
+}
+function mutiplicar(){
+    
+}
+function dividir(){
+    
+}
+function porcentaje(){
+    
+}
